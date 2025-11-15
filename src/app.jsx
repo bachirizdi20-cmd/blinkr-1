9bbcc15b48c5cf1cf452a5261eaa3151b7a254c1
bachirizdi20-cmd <bachirizdi20@gmail.com>
2025-11-15 12:01:19 +0100

    Update app.jsx
    Signed-off-by: bachirizdi20-cmd <bachirizdi20@gmail.com>

--- src/app.jsx
+++ src/app.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Auth';
-import SignUp from './pages/SignUp';
-import Home from './pages/Home';
-import Chat from './pages/Chat';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
 
-function App() {
+import Home from "./pages/Home"
+import Reels from "./pages/Reels"
+import Museum from "./pages/Museum"
+import Profile from "./pages/Profile"
+
+export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/auth" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/chat" element={<Chat />} />
-      </Routes>
+      <Navbar />
+      <div className="pt-20">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/reels" element={<Reels />} />
+          <Route path="/museum" element={<Museum />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
     </Router>
-  );
+  )
 }
-
-export default App;
