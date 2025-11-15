235d44ceff63d7ad3fa831a1c9fb77de74df5cb0
bachirizdi20-cmd <bachirizdi20@gmail.com>
2025-11-15 12:16:38 +0100

    Update supabaseClient.js
    Signed-off-by: bachirizdi20-cmd <bachirizdi20@gmail.com>

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,8 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js"
 
-// هذا الرابط والمفتاح يستخدمان مشروعًا عامًا للتجربة
-const supabaseUrl = 'https://kpxxogozkvemcxkhrxwp.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtweHhvZ296a3ZlbWN4a2hyeHdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTgwNzcyNzksImV4cCI6MjAxMzY1MzI3OX0.4V4J9D9mJx1Fh7P1yI05JrGkmzJxXUdSC9B6nO-5D9Y'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 
-// إنشاء عميل Supabase للتطبيق
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseKey)
